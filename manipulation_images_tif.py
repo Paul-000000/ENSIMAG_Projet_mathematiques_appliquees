40196ce2b714cf81711e770624ff8a982dd99642
Paul Bottner <paul.bottner@grenoble-inp.org>
2025-12-10 16:57:51 +0100

    amélioration du graphique de tests

--- manipulation_images_tif.py
+++ manipulation_images_tif.py
@@ -6,28 +6,19 @@ from numpy import ndarray
 from numpy.ma import MaskedArray
 
 
-
 def recuperer_images(mean_monthly : bool = False, zone : int = 2, selected_dates : list[str] = ['20210816', '20210828']) -> list[MaskedArray]:
-    
-    if not (8 >= zone >=1):
-        return images
     
     dir = f'./Data/Test_zone{zone}/{"STATS/MeanMonthly" if mean_monthly else "OASIS"}/'
     images = []
 
-    if mean_monthly:
+    if not (8 >= zone >=1):
+        return images
 
-        date_months=set()
-        for date in selected_dates:
-            date_months.add(date[:6])
-
-        selected_dates=date_months
-        
     for date in selected_dates:
 
         for file in os.listdir(dir):
 
-            if (date[:6] if mean_monthly else date) in file:
+            if date in file:
 
                 full_path = os.path.join(dir, file)
                 images.append(recuperer_image(full_path))
@@ -58,7 +49,7 @@ def test_segmentation(image : MaskedArray, fonction_segmentation : Callable[[Mas
     image_segmentee = fonction_segmentation(image)
     end = time.time()
 
-    print(f"segmentation terminée en : {round(end - start,3)} secondes")
+    print(f"fonction {fonction_segmentation.__name__} terminée en : {round(end - start,3)} secondes")
 
     _, plots = plt.subplots(1, 2, figsize=(10, 6))
 
@@ -77,10 +68,24 @@ def test_segmentation(image : MaskedArray, fonction_segmentation : Callable[[Mas
     plt.tight_layout()
     plt.show()
 
-def tests_segmentation(fonction_segmentation : Callable[[MaskedArray], ndarray], annee : int = 2021, mean_monthly : bool = False) -> None:
+def tests_segmentation(fonction_segmentation : Callable[[MaskedArray], ndarray], annee : int = 2021, mean_monthly : bool = False, resolution : int = 500) -> None:
 
     fig, plots = plt.subplots(16,12,figsize=(12, 8))
-    
+    mois_annee = [
+        "Janvier",
+        "Février",
+        "Mars",
+        "Avril",
+        "Mai",
+        "Juin",
+        "Juillet",
+        "Août",
+        "Septembre",
+        "Octobre",
+        "Novembre",
+        "Décembre"
+    ]
+
     plt.suptitle(f"Segmentation des Images pour l'Année {annee}", fontsize=16, fontweight='bold')
     temps_execution = []
 
@@ -119,14 +124,18 @@ def tests_segmentation(fonction_segmentation : Callable[[MaskedArray], ndarray],
         
         print()
 
-    fig.text(0.02, 0.5, 'Zone', ha='center', va='center', rotation='vertical', fontsize=12, fontweight='bold')
-    fig.text(0.5, 0.95, 'Mois', ha='center', va='center', fontsize=12, fontweight='bold')
+    for zone in range(1,9):    
+        fig.text(0.02, 0.97 - (zone / 8) * 0.9, f"Zone {zone}", ha='center', va='center', rotation='vertical', fontsize=9, fontweight='bold')
+    
+    for mois in range(1,13):
+        fig.text((mois / 12) * 0.98 -0.025, 0.93, f"{mois_annee[mois - 1]}", ha='center', va='center', fontsize=9, fontweight='bold')
 
     print(f"temps d'éxécution moyen de {fonction_segmentation.__name__} : {round(sum(temps_execution) / len(temps_execution),3)}")
     print(f"temps d'éxécution total de {fonction_segmentation.__name__} : {round(sum(temps_execution),3)}")
+    print("affichage et sauvegarde du graphique")
 
-    plt.tight_layout()
-    plt.savefig(f"{fonction_segmentation.__name__}_{annee}.png", dpi=500)
+    plt.tight_layout(rect=[0, 0, 1, 0.96])
+    plt.savefig(f"{fonction_segmentation.__name__}_{annee}.png", dpi=resolution)
     plt.show()
 
 
@@ -134,9 +143,9 @@ if __name__ == "__main__": # tests
 
     images = recuperer_images()
 
-    image = recuperer_image("./Data/Test_zone2/OASIS/s1a_fusion_ASC_161_20161228_oasis_VV_Offset55_Test_zone2.tif")
+    image = recuperer_image("./Data/Test_zone6/OASIS/s1a_fusion_ASC_161_20210118_oasis_VV_Offset55_Test_zone6.tif")
     
-    test_segmentation(image, lambda image : image)
+    #test_segmentation(image, lambda image : image)
     tests_segmentation(lambda image : image)
 
 
