ce917293be5131c7cc47270b73d25386c31ddd57
Paul Bottner <paul.bottner@grenoble-inp.org>
2025-12-12 14:17:16 +0100

    amélioration de random forest

--- codes/RF2.py
+++ codes/RF2.py
@@ -6,10 +6,10 @@ from sklearn.ensemble import RandomForestClassifier
 from manipulation_images_tif import *
 
 
-def extract_intensity_features(image):
+def extract_intensity_features(image : MaskedArray) -> ndarray:
     
     image_no_nan = image.filled(np.nan)
-    image_no_nan = np.nan_to_num(image_no_nan, nan=np.nanmean(image_no_nan))
+    image_no_nan = np.nan_to_num(image_no_nan, nan=0) # np.nanmean(image_no_nan)
 
     image_gray = image_no_nan.astype(float)
 
@@ -19,7 +19,7 @@ def extract_intensity_features(image):
 
     return image_normalisee.reshape(-1, 1)  # Feature = intensité
 
-def train_random_forest(images, masks):
+def train_random_forest(images : list[MaskedArray], masks : list[MaskedArray]) -> RandomForestClassifier:
 
     X_train = []
     y_train = []
@@ -43,20 +43,31 @@ def train_random_forest(images, masks):
         max_depth=10,
         random_state=0,
         n_jobs=-1,
-        verbose=2
+        verbose=0 #2
     )
     model.fit(X_train, y_train)
 
     return model
 
-
-def predict_segmentation(model, image):
+def predict_segmentation(model : RandomForestClassifier , image : MaskedArray) -> ndarray:
 
     x = extract_intensity_features(image)
     y_pred = model.predict(x)
+    
     return y_pred.reshape(image.shape)
 
-def load_training_data(annees : list[int] = [2021]):
+def load_training_images() -> tuple[list[MaskedArray],list[MaskedArray]]:
+
+    images = recuperer_images(mean_monthly=True, zone=2, selected_dates=['202101', '202102', '202103'])
+    masks = [
+        recuperer_image("./GroundTruth_DYN/Test_zone2/Var_202101.tif"),
+        recuperer_image("./GroundTruth_DYN/Test_zone2/Var_202102.tif"),
+        recuperer_image("./GroundTruth_DYN/Test_zone2/Var_202103.tif"),
+    ]
+
+    return images, masks
+
+def load_training_data(annees : list[int] = [2021]) -> tuple[list[MaskedArray],list[MaskedArray]]:
 
     images_x = []
     images_y = []
@@ -81,32 +92,18 @@ def load_training_data(annees : list[int] = [2021]):
 
     return images_x, images_y
 
-# joblib.dump(model, "mon_modele_random_forest.pkl")
+def save_model(model : RandomForestClassifier, filemane : str) -> None:
 
-# model = joblib.load("mon_modele_random_forest.pkl")
+    joblib.dump(model, f"{DOSSIER_SORTIE}/{filemane}.pkl")
+
+def load_model(filemane : str) -> RandomForestClassifier:
+
+    return joblib.load(f"{DOSSIER_SORTIE}/{filemane}.pkl")
+
+
 
 if __name__ == "__main__":
 
-    """
-    images = recuperer_images(
-        mean_monthly=True,
-        zone=2,
-        selected_dates=['202101', '202102', '202103']
-    )
-
-    masks = []
-
-    for i in range(len(images)) :
-        images[i] = images[i][0]
-        masks.append(images[i][1])
-        
-    
-    masks = [
-        recuperer_image("./GroundTruth_DYN/Test_zone2/Var_202101.tif"),
-        recuperer_image("./GroundTruth_DYN/Test_zone2/Var_202102.tif"),
-        recuperer_image("./GroundTruth_DYN/Test_zone2/Var_202103.tif"),
-    ]
-    """
 
     images, masks = load_training_data()
     print(f"nombre d'images : {len(images)}/{len(masks)}")
@@ -117,15 +114,15 @@ if __name__ == "__main__":
 
     print(f"temps d'entrainement {round(end-start,3)} secondes")
    
-    #image_test = recuperer_images(mean_monthly=True,zone=5,selected_dates=['202407'])[0]
+    
 
+    
     def segmentation_random_forest(image : np.ma.MaskedArray) -> np.ndarray:
 
         return predict_segmentation(model, image)
 
+    #image_test = recuperer_images(mean_monthly=True,zone=5,selected_dates=['202407'])[0]
     #test_segmentation(image_test, segmentation_random_forest)
 
-    #true_y=recuperer_image("./GroundTruth_DYN/Test_zone5/Var_202407.tif")
-
-    tests_segmentation(segmentation_random_forest,annee=2022)
-    moyenne_scores_annees(segmentation_random_forest, annees=[2021,2022,2023,2024])
+    #tests_segmentation(segmentation_random_forest,annee=2022)
+    #moyenne_scores_annees(segmentation_random_forest, annees=[2022,2023,2024])
